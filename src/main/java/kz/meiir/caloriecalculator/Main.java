b090723b6e21e2f89eb5c3f96bf88b5494dadbf5
Meiir Akhmetov <meir.akhmetov@gmail.com>
2022-04-19 16:36:03 +0600

    MAIN For TEST RUN

---
+++ src/main/java/kz/meiir/caloriecalculator/Main.java
@@ -0,0 +1,12 @@
+package kz.meiir.caloriecalculator;
+
+/**
+ * User: meirakhmetov
+ * Date: 19.04.2022
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, it is Calorie Calculator");
+    }
+
+}
